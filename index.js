f6e8ce02c27db99b006b79a174d5686fe0c1b620
Costin Andrei <90200368+costin-andrei@users.noreply.github.com>
2024-03-20 15:32:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,4 @@ export default (app) => {
     });
     return context.octokit.issues.createComment(pullRequestComment);
   });
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
